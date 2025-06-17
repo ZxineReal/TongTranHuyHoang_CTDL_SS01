392d52c0bebfdf6b6f77d2f8627664c6214dbcfb
ZxineReal <stargolem90@gmail.com>
2025-06-17 07:53:59 +0700

    update

--- PTIT_CNTT1_IT103_Session01_Bai07.c
+++ PTIT_CNTT1_IT103_Session01_Bai07.c
@@ -13,3 +13,15 @@ bool check1(int arr[], int n) {
 }
 /*Do phuc tap thoi gian: O(n ^2)*/
 /*Do phuc tap khong gian: O(1)*/
+
+// Cach 2:
+bool check2(int arr[], int n) {
+    int seenNumber[1001] = {0};
+    for (int i = 0; i < n; i++) {
+        if (seenNumber[arr[i]] == 1) {
+            return true;
+        }
+        seenNumber[arr[i]] = 1;
+    }
+    return false;
+}
